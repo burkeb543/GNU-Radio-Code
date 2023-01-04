5662ea603edb687383979ed592c805c56f487acb
Brian Burke <burkeb2@tcd.ie>
2023-01-04 13:09:53 +0000

    Improved OFDM
    Addition of files with more comprehensive OFDM implemnentation

--- OFDM_Receiver/OFDM_Receiver.py
+++ OFDM_Receiver/OFDM_Receiver.py
@@ -5,7 +5,8 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: First_GNU_File
+# Title: OFDM Loopback Example
+# Description: Transport packets.
 # GNU Radio version: 3.10.4.0
 
 from packaging.version import Version as StrictVersion
@@ -25,7 +26,6 @@ from gnuradio import qtgui
 from gnuradio.filter import firdes
 import sip
 from gnuradio import blocks
-import pmt
 from gnuradio import channels
 from gnuradio import digital
 from gnuradio import gr
@@ -35,17 +35,19 @@ import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
+from gnuradio.qtgui import Range, GrRangeWidget
+from PyQt5 import QtCore
 
 
 
 from gnuradio import qtgui
 
-class First_GNU_File(gr.top_block, Qt.QWidget):
+class OFDM_Receiver(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "First_GNU_File", catch_exceptions=True)
+        gr.top_block.__init__(self, "OFDM Loopback Example", catch_exceptions=True)
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("First_GNU_File")
+        self.setWindowTitle("OFDM Loopback Example")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -63,7 +65,7 @@ class First_GNU_File(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "First_GNU_File")
+        self.settings = Qt.QSettings("GNU Radio", "OFDM_Receiver")
 
         try:
             if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -76,13 +78,68 @@ class First_GNU_File(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 1200
-        self.packet_len = packet_len = 36
+        self.packet_len = packet_len = 50
+        self.samp_rate = samp_rate = 100000
+        self.noise_voltage = noise_voltage = 0.1
+        self.len_tag_key = len_tag_key = packet_len
+        self.freq_offset = freq_offset = 0
         self.fft_len = fft_len = 64
 
         ##################################################
         # Blocks
         ##################################################
+        self._noise_voltage_range = Range(0, 1, 0.01, 0.1, 200)
+        self._noise_voltage_win = GrRangeWidget(self._noise_voltage_range, self.set_noise_voltage, "Noise Amplitude", "dial", float, QtCore.Qt.Horizontal, "value")
+
+        self.top_layout.addWidget(self._noise_voltage_win)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+            1024, #size
+            samp_rate, #samp_rate
+            "Scope Plot", #name
+            1, #number of inputs
+            None # parent
+        )
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+
+        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_0.enable_tags(True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(True)
+        self.qtgui_time_sink_x_0.enable_grid(False)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+
+
+        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
+            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
+            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
         self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
             1024, #size
             window.WIN_BLACKMAN_hARRIS, #wintype
@@ -125,10 +182,14 @@ class First_GNU_File(gr.top_block, Qt.QWidget):
 
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self._freq_offset_range = Range(-3, 3, 0.01, 0, 200)
+        self._freq_offset_win = GrRangeWidget(self._freq_offset_range, self.set_freq_offset, "Frequency inner spacing)", "slider", float, QtCore.Qt.Horizontal, "value")
+
+        self.top_layout.addWidget(self._freq_offset_win)
         self.digital_ofdm_tx_0 = digital.ofdm_tx(
-            fft_len=fft_len,
-            cp_len=(fft_len//4),
-            packet_length_tag_key="len_tag_key",
+            fft_len=64,
+            cp_len=16,
+            packet_length_tag_key='packet_len',
             occupied_carriers=((-4,-3,-2,-1,1,2,3,4),),
             pilot_carriers=((-6,-5,5,6),),
             pilot_symbols=((-1,1,-1,1),),
@@ -140,9 +201,9 @@ class First_GNU_File(gr.top_block, Qt.QWidget):
             debug_log=False,
             scramble_bits=False)
         self.digital_ofdm_rx_0 = digital.ofdm_rx(
-            fft_len=fft_len, cp_len=(fft_len//4),
-            frame_length_tag_key='frame_'+"packet_len",
-            packet_length_tag_key="packet_len",
+            fft_len=64, cp_len=16,
+            frame_length_tag_key='frame_'+"rx_len",
+            packet_length_tag_key="rx_len",
             occupied_carriers=((-4,-3,-2,-1,1,2,3,4),),
             pilot_carriers=((-6,-5,5,6),),
             pilot_symbols=((-1,1,-1,1),),
@@ -153,39 +214,48 @@ class First_GNU_File(gr.top_block, Qt.QWidget):
             debug_log=False,
             scramble_bits=False)
         self.channels_channel_model_0 = channels.channel_model(
-            noise_voltage=0.0,
+            noise_voltage=0.1,
             frequency_offset=0.0,
             epsilon=1.0,
             taps=[1.0 + 1.0j],
             noise_seed=0,
             block_tags=False)
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_char*1, samp_rate,True)
-        self.blocks_stream_to_tagged_stream_0 = blocks.stream_to_tagged_stream(gr.sizeof_char, 1, 12, "len_tag_key")
-        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, 'C:\\Users\\burke\\OneDrive\\Desktop\\Uni\\4E2\\Image\\29477-1-cool-transparent-image.png', True, 0, 0)
-        self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, 'C:\\Users\\burke\\OneDrive\\Desktop\\Uni\\4E2\\Image_Out\\Test_Output.txt', False)
-        self.blocks_file_sink_0.set_unbuffered(True)
+        self.blocks_vector_source_x_0 = blocks.vector_source_b(range(0, 50), True, 1, [])
+        self.blocks_uchar_to_float_0 = blocks.uchar_to_float()
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
+        self.blocks_tag_debug_0 = blocks.tag_debug(gr.sizeof_char*1, 'Rx Packets', "")
+        self.blocks_tag_debug_0.set_display(False)
+        self.blocks_stream_to_tagged_stream_0 = blocks.stream_to_tagged_stream(gr.sizeof_char, 1, 50, "packet_len")
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle_0, 0))
         self.connect((self.blocks_stream_to_tagged_stream_0, 0), (self.digital_ofdm_tx_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.blocks_stream_to_tagged_stream_0, 0))
-        self.connect((self.channels_channel_model_0, 0), (self.digital_ofdm_rx_0, 0))
-        self.connect((self.digital_ofdm_rx_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.digital_ofdm_rx_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.blocks_uchar_to_float_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.blocks_vector_source_x_0, 0), (self.blocks_stream_to_tagged_stream_0, 0))
+        self.connect((self.channels_channel_model_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.digital_ofdm_rx_0, 0), (self.blocks_tag_debug_0, 0))
+        self.connect((self.digital_ofdm_rx_0, 0), (self.blocks_uchar_to_float_0, 0))
         self.connect((self.digital_ofdm_tx_0, 0), (self.channels_channel_model_0, 0))
-        self.connect((self.digital_ofdm_tx_0, 0), (self.qtgui_freq_sink_x_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "First_GNU_File")
+        self.settings = Qt.QSettings("GNU Radio", "OFDM_Receiver")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
 
         event.accept()
+
+    def get_packet_len(self):
+        return self.packet_len
+
+    def set_packet_len(self, packet_len):
+        self.packet_len = packet_len
+        self.set_len_tag_key(self.packet_len)
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -194,12 +264,25 @@ class First_GNU_File(gr.top_block, Qt.QWidget):
         self.samp_rate = samp_rate
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
         self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
-    def get_packet_len(self):
-        return self.packet_len
+    def get_noise_voltage(self):
+        return self.noise_voltage
 
-    def set_packet_len(self, packet_len):
-        self.packet_len = packet_len
+    def set_noise_voltage(self, noise_voltage):
+        self.noise_voltage = noise_voltage
+
+    def get_len_tag_key(self):
+        return self.len_tag_key
+
+    def set_len_tag_key(self, len_tag_key):
+        self.len_tag_key = len_tag_key
+
+    def get_freq_offset(self):
+        return self.freq_offset
+
+    def set_freq_offset(self, freq_offset):
+        self.freq_offset = freq_offset
 
     def get_fft_len(self):
         return self.fft_len
@@ -210,7 +293,7 @@ class First_GNU_File(gr.top_block, Qt.QWidget):
 
 
 
-def main(top_block_cls=First_GNU_File, options=None):
+def main(top_block_cls=OFDM_Receiver, options=None):
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
